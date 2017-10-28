3910353dbc96d56930ee155429b66c6c5bb1a6ec
Fabien JUIF <fabien.juif@gmail.com>
2017-10-28 14:07:23 +0200

    :sparkles: morphing on click (#8)

--- src/components/scene/scene.jsx
+++ src/components/scene/scene.jsx
@@ -1,24 +1,17 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { PropTypes as MobxPropTypes } from 'mobx-react'
+import { observer, PropTypes as MobxPropTypes } from 'mobx-react'
 import { interpolateAll, splitPathString } from 'flubber'
 import * as d3 from 'd3'
 import flatten from 'lodash/flatten'
 
-const animate = (sel, shapes) => {
-  const start = shapes[0]
-  const end = shapes[1]
-
-  const interpolator = interpolateAll(start, end, { single: true, maxSegmentLength: 5 })
+const animate = (sel, { previous, current }) => {
+  const interpolator = interpolateAll(previous, current, { single: true, maxSegmentLength: 5 })
 
   sel
     .transition()
     .duration(1000)
     .attrTween('d', () => interpolator)
-    // FIXME : mutation
-    .on('end', () => {
-      setTimeout(() => { sel.call(animate, shapes.slice(1).concat([shapes[0]])) }, 500)
-    })
 }
 
 class Scene extends Component {
@@ -56,12 +49,16 @@ class Scene extends Component {
         .map(draw => draw.map(complete))
     }
 
-    const normalizedDraws = normalize(this.props.draws.map(draw => draw.layers.toJSON()))
+    const { previous, current } = this.props
+
+    const layers = [previous, current].map(draw => draw.layers.toJSON())
+    const normalizedDraws = normalize(layers)
     const shapes = normalizedDraws.map(draw => splitPathString(draw[idx]))
+
     d3
       .select(e)
       .style('display', 'block')
-      .call(animate, shapes)
+      .call(animate, { previous: shapes[0], current: shapes[1] })
   }
 
   render() {
@@ -80,13 +77,15 @@ class Scene extends Component {
 Scene.propTypes = {
   style: PropTypes.object,
   className: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
-  draws: MobxPropTypes.observableArray,
+  previous: MobxPropTypes.observableObject,
+  current: MobxPropTypes.observableObject,
 }
 
 Scene.defaultProps = {
   style: {},
   className: '',
-  draws: undefined,
+  previous: undefined,
+  current: undefined,
 }
 
-export default Scene
+export default observer(Scene)

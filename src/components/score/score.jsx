06affe0fdaac2eea4c1c57c016b3274b1cee0d7c
Fabien JUIF <fabien.juif@zenika.com>
2017-10-28 12:33:55 +0200

    :sparkles: print score

---
+++ src/components/score/score.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+// import styles from './score.styles'
+
+const Score = ({ style, className, score }) => (
+  <div style={style} className={className}>
+    {score}
+  </div>
+)
+
+Score.propTypes = {
+  style: PropTypes.object,
+  className: PropTypes.oneOfType([PropTypes.string, PropTypes.object]),
+  score: PropTypes.number,
+}
+
+Score.defaultProps = {
+  style: {},
+  className: '',
+  score: undefined,
+}
+
+export default Score

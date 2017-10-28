fe9c01f484e2a7615c37d64a8cfdd503bc8c0fac
Fabien JUIF <fabien.juif@gmail.com>
2017-10-28 14:07:23 +0200

    :sparkles: morphing on click (#8)

--- src/components/scene/scene.container.js
+++ src/components/scene/scene.container.js
@@ -2,5 +2,6 @@ import { inject } from 'mobx-react'
 import Component from './scene'
 
 export default inject(({ store }) => ({
-  draws: store.draws,
+  current: store.current,
+  previous: store.previous,
 }))(Component)

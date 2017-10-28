e722a6822cf5662658598c25ac4711a6bede3cfc
Fabien JUIF <fabien.juif@zenika.com>
2017-10-28 14:55:31 +0200

    :sparkles: add a tweet button

--- src/screens/gameover/gameover.jsx
+++ src/screens/gameover/gameover.jsx
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-// import styles from './Gameover.styles'
+// import styles from './gameover.styles'
 
-const Gameover = ({ style, className, score }) => (
-  <div style={style} className={className}>
-    Well played, you did a {score} score !
+class Gameover extends Component {
+  tweetMount = (elm) => {
+    const text = `I scored ${this.props.score} on #iconofast, the flubber game created by @milletdelphine and @fabienjuif\n\n- cc @bdxio #bdxio`
 
-    Tweet about it : TODO
-  </div>
-)
+    const attach = () => {
+      if (!window.twttr || !window.twttr.widgets) {
+        window.requestAnimationFrame(attach)
+      } else {
+        window.twttr.widgets.createShareButton(
+          '/',
+          elm,
+          { text },
+        )
+      }
+    }
+
+    attach()
+  }
+
+  render() {
+    const { style, className, score } = this.props
+
+    return (
+      <div style={style} className={className}>
+        Well played, you did a {score} score !
+
+        Feel free to tweet about it : <div ref={this.tweetMount} />
+      </div>
+    )
+  }
+}
 
 Gameover.propTypes = {
   style: PropTypes.object,
